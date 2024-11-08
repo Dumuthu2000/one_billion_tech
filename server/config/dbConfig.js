2a20104574ab1da7d4e96c0610f1539298228460
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-08 22:41:43 +0530

    Initialize the database configurations by using sequelize orm and mysql

---
+++ server/config/dbConfig.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+
+//Create sequelize instance
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+});
+
+//Check is sequelize is authenticated or not
+sequelize.authenticate()
+    .then(()=>{
+        console.log("The database connection is succussfull")
+    }).catch((err)=>{
+        console.error("Error: ",err.message);
+    })
+
+
+module.exports = sequelize;
