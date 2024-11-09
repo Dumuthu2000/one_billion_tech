dcc5ebbd5064a0fbd3f781e910af55124a3c0443
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-09 17:58:30 +0530

    Create authMiddleare file to verify jwt token

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const verifyToken=(req, res, next)=>{
+    //Spliting token from request header
+    const token = req.headers['Authorization'].split(' ')[1];
+
+    //Check the token
+    if(!token){
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.user = decodedToken; //Store decoded user data into request object
+        next(); //Proceed to the next middleware or route
+    } catch (error) {
+        res.status(403).json({ message: 'Invalid or expired token.' });
+    }
+}
