c98a9bacd7cc87703c3df77a3d3a41382201dcb0
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-09 23:09:06 +0530

    Create task delete api route

--- server/routers/taskRoutes.js
+++ server/routers/taskRoutes.js
@@ -5,13 +5,13 @@ import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-//Create new task 
+//New task creating route api
 router.post('/create-task', verifyToken, validateTask, createTask);
-//Fetching all tasks where logged-in user 
+//Fetching all tasks where logged-in user route api
 router.get('/tasks', verifyToken, fetchTasks);
-//Update a task where logged-in user 
+//Update a task where logged-in user route api
 router.patch('/update-task/:id', verifyToken, validateTask, updateTask);
-//Delete a task where logged-in user 
+//Delete a task where logged-in user route api
 router.delete('/delete-task/:id', verifyToken, deleteTask);
 
 export default router;
