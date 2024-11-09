c0a8df476f579abe473d3b050b1c28f0e2c7b24f
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-09 09:38:41 +0530

    Make change the code structure

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 //Create sequelize instance
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+    } 
+);
 
 //Check is sequelize is authenticated or not
 sequelize.authenticate()
