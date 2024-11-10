609ad3a05e585be6ef2eb5c6cae8e861035ce192
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-10 16:37:08 +0530

    Change password shift to the useController file and set as userRoute api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -168,38 +168,3 @@ export const resetPassword=async(req, res, next)=>{
         next(error);
     }
 }
-
-//Change password functionality
-export const changePassword=async(req, res, next)=>{
-    const{currentPassword, newPassword} = req.body;
-    const userId = req.user.id;
-
-    if(!currentPassword || !newPassword){
-        return next(new CustomError('Current password and new password are required', 400));
-    }
-
-    try {
-        const user = await User.findOne({where: {userId}});
-
-        if(!user){
-            throw new CustomError("User is not found", 404);
-        }
-
-        //Check if the current password is match or not
-        const isMatch = await verifyHashingData(currentPassword, user.password);
-        if(!isMatch){
-            throw new CustomError('Current password is invalid', 403);
-        }
-
-        //Hashing new password
-        const hashedPassword = await hashingData(newPassword);
-        await user.update({password: hashedPassword});
-
-        return res.status(200).json({
-            status: 'success',
-            message: 'Password change successfully',
-        })
-    } catch (error) {
-        next(error);
-    }
-}
--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -1,3 +1,5 @@
-import express from 'express';
+import { changePassword } from '../controllers/userController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
-const router = express.Router();
+//Change password from email route
+router.patch('/change-password', verifyToken, changePassword);
