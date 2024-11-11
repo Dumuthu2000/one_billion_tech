085dce564478a1a755407165fc44c1820b510ec0
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-11 21:36:36 +0530

    Add new task api is added to useTodo custom hook

--- client/src/hooks/useTodo.js
+++ client/src/hooks/useTodo.js
@@ -6,6 +6,7 @@ const useTodo = () => {
   const [error, setError] = useState(null); // Use null instead of false for error state
   const [todoList, setTodoList] = useState([]);
 
+  //Fetching TO-DO tasks that logged-in user
   const fetchTodoList = async () => {
     setLoading(true);
     setError(null); // Reset error state before fetching
@@ -17,13 +18,41 @@ const useTodo = () => {
 
       const { data } = response;
       setTodoList(data.tasks); // Store the fetched tasks in todoList
+
+      setLoading(false);
     } catch (error) {
       setError(error.response?.data?.message || 'Something went wrong');
       setLoading(false);
     } 
   };
 
-  return { loading, error, fetchTodoList, todoList };
+  //Create a new TO-DO task
+  const addTodo=async(formData)=>{
+    setLoading(true);
+    setError(null); 
+
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/task/tasks`,
+        formData, {
+          withCredentials: true,
+        }
+      );
+
+      const { data } = response;
+
+      //Add new task to the existing list of tasks
+      setTodoList((prevTodoList)=>{
+        [...prevTodoList, data.data]
+      });
+
+      setLoading(false);
+    } catch (error) {
+      setError(error.response?.data?.message || 'Something went wrong');
+      setLoading(false);
+    }
+  }
+
+  return { loading, error, fetchTodoList, addTodo, todoList };
 };
 
 export default useTodo;
