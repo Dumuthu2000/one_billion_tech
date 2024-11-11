b87d7974120941cf41a6adecc82f34d1c9079ebb
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-11 13:12:29 +0530

    Store user data gloabally when login

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -4,9 +4,11 @@ const AuthContext = createContext();
 
 export const AuthProvider=({children})=>{
     const[isLoggedIn, setIsLoggedIn] = useState(false);
+    const[user, setUser] = useState(null);
 
-    const login=()=>{
+    const login=(userData)=>{
         setIsLoggedIn(true);
+        setUser(userData); //Store user data globaly
     }
 
     const logout=()=>{
@@ -14,7 +16,7 @@ export const AuthProvider=({children})=>{
     }
 
     return(
-        <AuthContext.Provider value={{isLoggedIn, login, logout}}>
+        <AuthContext.Provider value={{isLoggedIn, login, logout, user}}>
             {children}
         </AuthContext.Provider>
     )
