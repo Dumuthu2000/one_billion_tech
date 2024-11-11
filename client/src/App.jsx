c8a589cc6a6b11640ed0486d4a01da50e254de55
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-11 09:56:10 +0530

    Remove TodoForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
 import Profile from './pages/Profile'
-import TodoForm from './components/TodoForm'
 import Login from './pages/Login';
 import Signup from './pages/Signup'
 
@@ -18,7 +17,6 @@ const App = () => {
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/profile' element={<Profile/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/create-todo' element={<TodoForm/>}/>
         </Routes>
       </BrowserRouter>
     </div>
