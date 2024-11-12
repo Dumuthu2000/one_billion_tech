a159f031f0d9e8382d21d38eff7f226f9e51ba82
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-12 17:30:25 +0530

    Enhancing ui of each

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -22,7 +22,7 @@ const ForgotPassword = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8" >
       <div className="max-w-md w-full mx-auto">
         <div className="bg-white rounded-lg shadow-md p-8">
           {/* Back to Login Link */}
